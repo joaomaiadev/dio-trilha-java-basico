6eba6b7a8c1a4709266adf150d0560971f1faa2f
Joao <joao.vit.dev@gmail.com>
2024-08-01 21:19:21 -0300

    Operadores criados / Tipos de variáveis atualizados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+        char sexo = 'M';  //só deixa Caractere
+        boolean passou = false;
     }
 }
